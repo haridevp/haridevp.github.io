d6602c38e67b56132d38faf4b63958aab91774f8
Haridev P <79982347+haridevp@users.noreply.github.com>
2025-07-15 22:54:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const inputfield = document.getElementById("dummyKeyboard");
 
 inputfield.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
-    let input = e.target.value;
+    let input = e.target.value.trim();
     input = input.toLowerCase();
     if (input.length === 0) {
       return;
